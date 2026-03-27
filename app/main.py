d3fd71b5b0e17d4400735e20eb0b405c25f43fa8
Yasser Alghamdi <ya3sa6@gmail.com>
2026-03-28 02:42:35 +0300

    implement genderize endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi.responses import JSONResponse
+from app import service
 from . import app
 
 @app.get("/genderize")
 async def genderize(name: str):
-    return JSONResponse(content={})
+    return JSONResponse(content=service.genderize(name))
