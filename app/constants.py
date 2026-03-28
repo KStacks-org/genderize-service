d1647c65526cf6b185201a69e23b8cffcfb6b70f
Yasser Alghamdi <ya3sa6@gmail.com>
2026-03-28 20:41:32 +0300

    Unifying the source of constant variables

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,10 @@
+import os
+
 API_URL = "https://api.genderize.io/"
 API_SOURCE_NAME = "api.genderize.io"
+GENDERIZE_API_KEY = os.getenv("GENDERIZE_API_KEY")
 
 DEFAULT_DATA_PATH = "./data/default_data.csv"
 DEFAULT_DATA_SOURCE_NAME = "reviewed_dataset"
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
--- app/extensions.py
+++ app/extensions.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+from app.constants import DATABASE_URL
 
 # import redis
 # import fakeredis
 
 # APP_ENV = os.getenv("APP_ENV", "dev")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
 # REDIS_URL = os.getenv("REDIS_URL", None)
 
 
