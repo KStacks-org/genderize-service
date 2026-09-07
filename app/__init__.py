573f7e5f1ab3f66531df4b0f10a871ab8ba0ddc1
Yasser Alghamdi <ya3sa6@gmail.com>
2026-09-07 23:20:17 +0300

    update logging configuration and add request logging middleware

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from .database import init_db
 app = FastAPI(
     title="Genderize Service",
     description="A service to determine the gender of a name using both a local dataset and an external API.",
-    version="1.0.0",
+    version="1.0.1",
 )
 
 init_db()
