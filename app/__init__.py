f1e8d17be050a7f0a943e864316d90f34a5a8af0
Yasser Alghamdi <ya3sa6@gmail.com>
2026-09-07 23:40:10 +0300

    fix circular import error

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from .database import init_db
-
+from .logging import setup_logging
 app = FastAPI(
     title="Genderize Service",
     description="A service to determine the gender of a name using both a local dataset and an external API.",
     version="1.0.1",
 )
 
+setup_logging(app)
 init_db()
 
